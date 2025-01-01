709b45b6a9c614d5f20803019e0f648c28c402ab
fileuplaoderbot <tparveen2728@gmail.com>
2025-01-01 12:46:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://")
+URL = environ.get("URL", "https://crooked-urial-moviehorn1-ecdfe1d9.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
